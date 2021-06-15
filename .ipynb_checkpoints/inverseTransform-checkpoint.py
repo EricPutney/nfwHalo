1fd88826f8d9f9ed44a1076e463eaa4ebf74454c
Eric Putney <ericrputney@gmail.com>
2021-06-14 23:36:48 -0700

    Cleaned up nfwMockDataGenerator, nfwFunctions, and inverseTransform

--- .ipynb_checkpoints/inverseTransform-checkpoint.py
+++ .ipynb_checkpoints/inverseTransform-checkpoint.py
@@ -6,9 +6,7 @@ from scipy.interpolate import interp1d
 
 def inverseTransformSampling(CDF, bracket, x0=None, x1=None):
     """
-    Simple implementation of inverseTransformSampling. Currently a little slow since I need to loop though each sample instead of doing it in one step that can be parallelized. You win some, you lose some. Anyways, this takes any cumulative distribution function (CDF), subtracts some random offset from it between 0 and 1, and then finds the root of that offset CDF. When you repeat this process and record the roots, you actually recover the original distribution function and can draw from it truly at random.
-
-    The CDF(bracket[1]) is multiplied onto the random number to accomodate CDFs that aren't properly normalized. The size of the random number can now never be larger than the max value of the CDF.
+    Simple implementation of inverseTransformSampling. This takes any cumulative distribution function (CDF), subtracts some random offset between 0 and max(CDF) from it, and then finds the root of that offset CDF. When you repeat this process and record the roots, you recover the original distribution function and are sampling it directly.
     """
     cdfmax = CDF(bracket[1])
     randNum = cdfmax*np.random.default_rng().uniform(0,1,1)[0]
@@ -39,19 +37,4 @@ def inverseTransformSamplingVelocity(nTracers, haloAttributes, radiiNorm, tabNor
         CDFsubRandomNumber = lambda x: NFW.speedCDF(radiiNorm[i],x,haloAttributes,tabNormRadii,speedCubicSplines) - randNum
         resultList.append(optimize.root_scalar(CDFsubRandomNumber , bracket = bracket, method='brentq').root)
     return resultList
-
-#def inverseTransformSamplingVelocity(nTracers, haloAttributes, radiiNorm):
-#    """
-#    Specially built ITS method for the cumulative distribution function for picking a speed.
-#    """
-#    resultList = []
-#    for i in range(nTracers):
-#        disp = NFW.radialVelocityDispersion(radiiNorm[i], haloAttributes)
-#        bracket=(0,NFW.maxSpeed(radiiNorm[i], haloAttributes))
-#        cdfmax = NFW.speedCDFcallByDisp(bracket[1], disp)
-#        randNum = cdfmax*np.random.default_rng().uniform(0,1,1)[0]
-#        CDFsubRandomNumber = lambda x: NFW.speedCDFcallByDisp(x, disp) - randNum
-#        resultList.append(optimize.root_scalar(CDFsubRandomNumber , bracket = bracket, method='brentq', x0=disp).root)
-#    return resultList
-    
     
